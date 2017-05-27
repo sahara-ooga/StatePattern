61c0e1cf0ec029cdb4753863fb2d173f29441aeb
yuu ogasawara <y.ogasawara@st-ventures.jp>
2017-05-27 18:05:06 +0900

    参考ページのように、アプリの起動から、画面遷移までの流れを実装

--- StatePattern.playground/Pages/Model Controllerの考え方を導入してみる.xcplaygroundpage/Contents.swift
+++ StatePattern.playground/Pages/Model Controllerの考え方を導入してみる.xcplaygroundpage/Contents.swift
@@ -1,6 +1,23 @@
 //: [Previous](@previous)
+//: 参考：https://code.tutsplus.com/tutorials/the-right-way-to-share-state-between-swift-view-controllers--cms-28474
 
 import UIKit
+
+//アプリ起動時にstateを設定する
+//Reachabilityでできるようにしてもいいかも
+
+class AppDelegate:UIResponder,UIApplicationDelegate{
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey : Any]? = nil) -> Bool {
+        if let firstViewController = window?.rootViewController as? FirstViewController{
+            firstViewController.stateController = StateController()
+        }
+        
+        return true
+    }
+}
 
 class StateController{
     var state:State = Default()
@@ -14,7 +31,67 @@ class StateController{
     }
     
     func responseToRequest() -> String {
-        return self.state.responseToRequest() 
+        return self.state.responseToRequest()
     }
 }
+
+//:通信には専用のControllerが必要。通信した結果をmodel controllerに渡す
+//class NewworkingController{
+//    var stateController:StateController
+//    
+//    init(stateController:StateController) {
+//        self.stateController = stateController
+//    }
+//    
+//    func fetchData() {
+//        //fetch data
+//        
+//        self.stateController.state = Lording()
+//    }
+//}
+
+class FirstViewController:UIViewController {
+    var stateController:StateController!
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        let state = stateController.state
+        print(state.stateDescription())
+        print(state.responseToRequest())
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let secondViewController = segue.destination as? SecondViewController{
+            secondViewController.stateController = self.stateController
+        }
+    }
+}
+
+class SecondViewController:UIViewController{
+    var stateController:StateController!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let state = stateController.state
+        print(state.stateDescription())
+        print(state.responseToRequest())
+    }
+    
+    
+    func someNetworkRequest(){
+        let newOnlineState = Online()
+        stateController.change(state:newOnlineState)
+    }
+}
+
+//FirstViewControllerが表示される
+let vc = FirstViewController()
+vc.stateController = StateController()
+vc.viewWillAppear(true)
+
+//通信状態が変化する
+vc.stateController.change(state: Offline())
+vc.viewWillAppear(false)
+
 //: [Next](@next)
