8069be5bc6cdc926073ffd27964da900ca62b457
yuu ogasawara <y.ogasawara@st-ventures.jp>
2017-05-28 20:46:55 +0900

    新ページを追加、State.swiftを参照フォルダに追加

---
+++ StatePattern.playground/Pages/プロトコル化.xcplaygroundpage/Sources/State.swift
@@ -0,0 +1,69 @@
+public protocol State{
+    func stateDescription() -> String
+    func responseToRequest() -> String
+}
+
+public struct Default:State{
+    public func stateDescription() -> String {
+        return "デフォルトの状態です"
+    }
+    
+    public func responseToRequest() -> String {
+        return "待機状態です"
+    }
+    
+    public init(){}
+
+}
+
+public struct Online:State{
+    public func stateDescription() -> String {
+        return "オンラインです"
+    }
+    
+    public func responseToRequest() -> String {
+        return "今から取得を開始します"
+    }
+    
+    public init(){}
+
+}
+
+public struct Lording:State{
+    public func stateDescription() -> String {
+        return "ロード中です"
+    }
+    
+    public func responseToRequest() -> String {
+        return "すでにロード中です"
+    }
+    
+    public init(){}
+
+}
+
+public struct Offline:State{
+    public func stateDescription() -> String {
+        return "オフラインです"
+    }
+    
+    public func responseToRequest() -> String {
+        return "オフライン中なので、ダウンロードできません"
+    }
+    
+    public init(){}
+
+}
+
+public struct NetworkError:State{
+    public func stateDescription() -> String {
+        return "ネットワークエラーが発生しました"
+    }
+    
+    public func responseToRequest() -> String {
+        return "エラーが発生しました"
+    }
+    
+    public init(){}
+
+}
